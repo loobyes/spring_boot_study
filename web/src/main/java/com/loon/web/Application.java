af4e3f9beb716a2fa979cfaa09aaabd417b6c11d
loobyes <loobyes@163.com>
2018-09-29 16:30:41 +0800

    first commit

---
+++ web/src/main/java/com/loon/web/Application.java
@@ -0,0 +1,16 @@
+package com.loon.web;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author longchao
+ * @date 2018-09-29
+ * @description
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ web/src/main/java/com/loon/web/controller/KafkaTestController.java
@@ -0,0 +1,13 @@
+package com.loon.web.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author longchao
+ * @date 2018-09-29
+ * @description
+ */
+@RestController
+public class KafkaTestController {
+
+}
